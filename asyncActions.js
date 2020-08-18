de1c2d4db54400941cb7b80138cb8a4450358ebd
rabobahago <rabobahago@gmail.com>
2020-08-18 17:39:21 +0100

    async actions codes added

---
+++ asyncActions.js
@@ -0,0 +1,56 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
+const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
+const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
+
+const initialState = {
+    loading: false,
+    users: [],
+    error: ''
+}
+
+const fetchUserSRequest = ()=>{
+    return{
+        type: FETCH_USERS_REQUEST
+    }
+}
+const fetchUserSuccess = users =>{
+    return{
+        type: FETCH_USERS_SUCCESS,
+        payload: users
+    }
+}
+const fetchUserSFailure = error=>{
+    return{
+        type: FETCH_USERS_FAILURE,
+        payload: error
+    }
+}
+
+
+const reducer = (state = initialState, action) =>{
+    switch(action.type){
+        case FETCH_USERS_REQUEST:
+            return{
+                ...state, 
+                loading: true
+            }
+        case FETCH_USERS_SUCCESS:
+            return{
+                ...state,
+                loading: false,
+                users: action.payload,
+                error: ''
+            }
+        case FETCH_USERS_FAILURE:
+            return{
+                ...state,
+                loading: false,
+                users: [],
+                error: action.payload
+            }
+    }
+}
+
+const store = createStore(reducer);
